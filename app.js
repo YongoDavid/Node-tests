8717c94299cf2837ebfff68c2beb37dab8c55182
Yongo David <davidyongo9@gmail.com>
2024-02-06 13:21:44 +0100

    express view engines update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // register view engine 
-app.use('view engine' , 'ejs ')
+app.set('view engine' , 'ejs')
 
 // listen for requesst 
 app.listen(3000);
@@ -12,19 +12,19 @@ app.listen(3000);
     // getting response in express 
 app.get('/' , (req , res ) => {
     // res.send('home page</p>')
-    res.sendFile('/views/index.html' , {root: __dirname}); 
-})
-app.get('/about' , (req , res ) => {
-    // res.send('about page</p>')
-    res.sendFile('views/about.html' , {root: __dirname}); 
-})
-
-// REDIRECTS IN EXPRESS
-app.get('/about-me' , (req , res)=> {
-    res.redirect('about')
+    res.render('index'); 
 });
-
-// 404 page  
-app.use((req , res) => {
-    res.status(400).sendFile('views/404.html' , {root: __dirname})
+app.get('/about' , (req,res)=> {
+    res.render('about')
 });
+app.get('/blogs/create' , (req , res)=> {
+    res.render('create')
+}); 
+// // redirects 
+// app.get('/about-us' , (req,res)=>{
+//     res.redirect('about')
+// }); 
+app.use((req,res)=>{
+    res.status(404).render('404')
+}); 
+
