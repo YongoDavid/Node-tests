8df4aa9086f794fdfd13022f883874ab89f35f6b
Yongo David <davidyongo9@gmail.com>
2024-02-06 14:53:21 +0100

    ejs partials

--- app.js
+++ app.js
@@ -11,20 +11,26 @@ app.listen(3000);
 
     // getting response in express 
 app.get('/' , (req , res ) => {
-    // res.send('home page</p>')
-    res.render('index'); 
+
+    // using ejs to export coontent dynamically 
+    const blogs = [
+        {title : 'David finds cars' , snippet: 'lorem i dont know you dont know'},
+        {title : 'David loves cars' , snippet: 'lorem i dont know you dont know'},
+        {title : 'David drives cars' , snippet: 'lorem i dont know you dont know'},
+    ];
+    res.render('index' , {title : 'Home page' , blogs}); 
 });
 app.get('/about' , (req,res)=> {
-    res.render('about')
+    res.render('about' , {title : 'About page'})
 });
 app.get('/blogs/create' , (req , res)=> {
-    res.render('create')
+    res.render('create' , {title: 'Creat a new blog'})
 }); 
 // // redirects 
 // app.get('/about-us' , (req,res)=>{
 //     res.redirect('about')
 // }); 
 app.use((req,res)=>{
-    res.status(404).render('404')
+    res.status(404).render('404' , {title : "Error page"})
 }); 
 
