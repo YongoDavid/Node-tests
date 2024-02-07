56399ee93c27cdbad43f99382d61e994773a3c93
Yongo David <davidyongo9@gmail.com>
2024-02-07 13:27:36 +0100

    middleware

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.set('view engine' , 'ejs')
 // listen for requesst 
 app.listen(3000);
 
+// MIDDLEARES 
+app.use((req,res , next)=> {
+    console.log('new request made');
+    console.log('host: ' , req.hostname);
+    console.log('path:' , req.path);
+    console.log('method:' , req.method);
+    next();
+});
+app.use((req,res , next)=> {
+    console.log('in the next middleware');
+    next();
+});
+
     // getting response in express 
 app.get('/' , (req , res ) => {
 
