0db6b25d63567dda436792c375641ca9e05b1cc8
Yongo David <davidyongo9@gmail.com>
2024-02-08 16:05:08 +0100

    model ans schema update

--- models/blogs.js
+++ models/blogs.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose'); 
 const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        // setting the time of data we want from each property to be 
+        type: String,
+        required: true
+    },
+    snippet:{
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    // timestamp as an options object 
+}, {timestamps: true});
+
+// This is how you create your model around the schema 
+const Blog = mongoose.model('Blog' , blogSchema);
+// Export the model to be able to use anyhere is your code 
+module.exports(Blog);
