ac53df1f663ed4411f7cb97b98dacf0c86cab91b
Yongo David <davidyongo9@gmail.com>
2024-02-13 14:13:20 +0100

    Controllers , Routes

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -1,59 +1,11 @@
 const express = require('express')
-// importing model 
-const Blog = require('../models/blogs');
-const router = express.Router()
-
-
+const blogController = require('../controllers/blogController')
+const router = express.Router(); 
 // Blog routes 
-
-
-router.get('/blogs', (req,res)=>{
-    Blog.find().sort({createdAt: -1})
-    .then((result)=>{
-        res.render('index' , {title: 'All Blogs' , blogs: result})
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
-// creating a new instance for new data and saving to database 
-router.post('/blogs',(req,res)=>{
-    const blog = new Blog(req.body);
-    
-    blog.save()
-        .then((result)=>{
-            res.redirect('/blogs');
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
-router.get('/blogs/create' , (req , res)=> {
-    res.render('create' , {title: 'Creat a new blog'})
-}); 
-// get a particular blog by its id
-router.get('/blogs/:id',(req,res)=>{
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result =>{
-        // render the file you want your selected id results to appear in 
-        res.render('details' , {blog: result , title: 'Blog  Detials'})
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
-// deletle a particular blog by its id 
-router.delete('/blogs/:id' , (req,res)=>{
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-    // after the delete is complete we sent a json and redirect the json 
-    .then(result =>{
-        res.json({ redirect: '/blogs'})
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-})
+router.get('/blogs', blogController.blog_index);
+router.post('/blogs', blogController.blog_create_post)
+router.get('/blogs/create' , blogController.blog_create_get); 
+router.get('/blogs/:id', blogController.blog_detials);
+router.delete('/blogs/:id' , blogController.blog_delete )
  
 module.exports = router
---
+++ controllers/blogController.js
@@ -0,0 +1,64 @@
+// ALL VARIABLES FOR ROUTE FUNCTIONS WILL STAY HERE 
+// importing model 
+const Blog = require('../models/blogs');
+const blog_index = (req,res) =>{
+    Blog.find().sort({createdAt: -1})
+    .then((result)=>{
+        res.render('index' , {title: 'All Blogs' , blogs: result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+
+// get a particular blog by its id 
+const blog_detials = (req,res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result =>{
+        // render the file you want your selected id results to appear in 
+        res.render('details' , {blog: result , title: 'Blog  Detials'})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+const blog_create_get = (req,res) =>{
+    res.render('create' , {title: 'Create a new blog'})
+}
+
+// creating a new instance for new data and saving to database 
+const blog_create_post = (req,res) =>{
+    const blog = new Blog(req.body);
+    
+    blog.save()
+        .then((result)=>{
+            res.redirect('/blogs');
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+
+// deletle a particular blog by its id 
+const blog_delete = (req,res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    // after the delete is complete we sent a json and redirect the json 
+    .then(result =>{
+        res.json({ redirect: '/blogs'})
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+
+module.exports ={
+    blog_index,
+    blog_detials,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
